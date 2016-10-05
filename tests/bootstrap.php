693e9eca51c850e70c59a05d11f43ec47e10f2b5
Antonino visalli <antonino.visalli@gmail.com>
2016-10-05 15:24:20 +0200

    Fix test config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * ACL Manager
- *
- * @link        https://github.com/ripaclub/aclman
- * @copyright   Copyright (c) 2014, RipaClub
- * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
- */
+
 chdir(__DIR__);
 
 if (!file_exists('../vendor/autoload.php')) {
